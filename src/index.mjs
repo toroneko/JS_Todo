38ff6d8f937b7726d386e7b9fac5f5ebce1d53b7
toroneko <167988065+toroneko@users.noreply.github.com>
2024-05-28 14:52:19 +0000

    JSでで追加・削除・完了・戻す機能を追加

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,60 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createIncompleteTodo(inputText);
+};
+// 渡された引数を基に未完了のTODOを作成する関数
+const createIncompleteTodo = (todo) => {
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = todo;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      backButton.closest("li").remove();
+    });
+    moveTarget.firstElementChild.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(moveTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
